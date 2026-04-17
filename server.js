ee987f14f5e7c8c2e20ecdc72fb85d5dcf186062
kariukijames052001-ux <kariukijames052001@gmail.com>
2026-04-17 15:56:12 +0300

    Initialize Express server with basic route

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("DEL'S DEN API LIVE 🚀");
+});
+
+app.listen(3000, () => console.log("Server running"));
