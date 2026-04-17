c0cd45574b0f6338659090319ed0b3c4a8f3720d
kariukijames052001-ux <kariukijames052001@gmail.com>
2026-04-17 17:06:28 +0300

    Update server to listen on all network interfaces

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
     res.send("DEL'S DEN API LIVE 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server running on port " + PORT);
+});
