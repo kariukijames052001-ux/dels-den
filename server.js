2c1b1f6de5fedb7cbd72812972d9824e49cdcd05
kariukijames052001-ux <kariukijames052001@gmail.com>
2026-04-23 17:19:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+app.post('/order', (req, res) => {
+  const { phone } = req.body;
 
+  console.log("New order from:", phone);
+
+  res.json({
+    success: true,
+    message: "Order received ✅. Payment request coming soon..."
+  });
+});
 app.listen(PORT, '0.0.0.0', () => {
   console.log("Server running on port " + PORT);
 });
